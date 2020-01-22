bf56772b214383632609d80ea473fff3a96739bd
Juan Jesús Fernández Madrid <juangefm@gmail.com>
2020-01-22 23:32:44 +0100

    manage ui from activity

--- app/src/main/java/com/apiumhub/androidcourse/MainActivity.kt
+++ app/src/main/java/com/apiumhub/androidcourse/MainActivity.kt
@@ -2,11 +2,27 @@ package com.apiumhub.androidcourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var innerCounter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val counter: TextView = findViewById<TextView>(R.id.counter)
+        val button: Button = findViewById<Button>(R.id.button)
+
+        counter.setText(innerCounter.toString())
+
+        button.text = "Incremento"
+        button.setOnClickListener {
+            innerCounter++
+            counter.setText(innerCounter.toString())
+        }
     }
 }
