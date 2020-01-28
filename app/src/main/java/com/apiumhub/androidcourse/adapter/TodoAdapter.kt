cd1ba62bba68ed3c1b8f7cf233f13ed45b84bf39
Juan Jesús Fernández Madrid <juangefm@gmail.com>
2020-01-28 20:45:07 +0100

    add date formatter

--- app/src/main/java/com/apiumhub/androidcourse/adapter/TodoAdapter.kt
+++ app/src/main/java/com/apiumhub/androidcourse/adapter/TodoAdapter.kt
@@ -6,8 +6,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.apiumhub.androidcourse.R
 import kotlinx.android.synthetic.main.row_element.view.*
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class TodoAdapter(private val data: List<TodoItem>) : RecyclerView.Adapter<TodoViewHolder>() {
+
+    private val dateFormat = SimpleDateFormat("dd/MM/yyyy")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.row_element, parent, false)
@@ -21,6 +25,6 @@ class TodoAdapter(private val data: List<TodoItem>) : RecyclerView.Adapter<TodoV
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val todoItem: TodoItem = data[position]
         holder.itemView.title.text = todoItem.title
-        holder.itemView.date.text = todoItem.title
+        holder.itemView.date.text = dateFormat.format(Date(todoItem.date))
     }
 }
