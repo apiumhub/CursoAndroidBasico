dd19099dd06d61764c8b97658253b4b5096ae353
Juan Jesús Fernández Madrid <juangefm@gmail.com>
2020-01-28 20:44:53 +0100

    allow add elements to data

--- app/src/main/java/com/apiumhub/androidcourse/TodoListActivity.kt
+++ app/src/main/java/com/apiumhub/androidcourse/TodoListActivity.kt
@@ -1,16 +1,41 @@
 package com.apiumhub.androidcourse
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.apiumhub.androidcourse.adapter.TodoAdapter
+import com.apiumhub.androidcourse.adapter.TodoItem
 
 class TodoListActivity : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
+    private lateinit var viewLayoutManager: RecyclerView.LayoutManager
+    private lateinit var todoAdapter: TodoAdapter
+    private val data: MutableList<TodoItem> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recyclerView = findViewById(R.id.recyclerView)
+
+        viewLayoutManager = LinearLayoutManager(this)
+        todoAdapter = TodoAdapter(data)
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+            setHasFixedSize(true)
+            layoutManager = viewLayoutManager
+            adapter = todoAdapter
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val elements: Array<TodoItem> = arrayOf(
+            TodoItem("Elemento 1", System.currentTimeMillis()),
+            TodoItem("Elemento 2", System.currentTimeMillis()),
+            TodoItem("Elemento 3", System.currentTimeMillis())
+        )
+        data.addAll(elements)
+        todoAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/apiumhub/androidcourse/adapter/TodoAdapter.kt
+++ app/src/main/java/com/apiumhub/androidcourse/adapter/TodoAdapter.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.apiumhub.androidcourse.R
 import kotlinx.android.synthetic.main.row_element.view.*
 
-class TodoAdapter(private val data: Array<TodoItem>) : RecyclerView.Adapter<TodoViewHolder>() {
+class TodoAdapter(private val data: List<TodoItem>) : RecyclerView.Adapter<TodoViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.row_element, parent, false)
