f9c3c4b8558fb0a30e0e82e5ab2eb371a981e60e
Juan Jesús Fernández Madrid <juangefm@gmail.com>
2020-01-28 20:00:29 +0100

    add recycler view to main activity

--- app/src/main/java/com/apiumhub/androidcourse/MainActivity.kt
+++ app/src/main/java/com/apiumhub/androidcourse/MainActivity.kt
@@ -2,11 +2,15 @@ package com.apiumhub.androidcourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recyclerView)
     }
 }
