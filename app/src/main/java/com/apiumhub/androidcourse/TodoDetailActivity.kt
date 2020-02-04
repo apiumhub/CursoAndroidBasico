056a0e2e5c6b0326d0b2d88bca75093e2b3f5b50
Juan Jesús Fernández Madrid <juangefm@gmail.com>
2020-02-04 21:20:10 +0100

    add empty todo detail activity

---
+++ app/src/main/java/com/apiumhub/androidcourse/TodoDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.apiumhub.androidcourse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TodoDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo_detail)
+    }
+}
