2cd7e0927f93bdde11123ee3b960a538db69b868
Juan Jesús Fernández Madrid <juangefm@gmail.com>
2020-02-04 21:25:07 +0100

    add parcel implementation to TodoItem

--- app/src/main/java/com/apiumhub/androidcourse/adapter/TodoItem.kt
+++ app/src/main/java/com/apiumhub/androidcourse/adapter/TodoItem.kt
@@ -1,3 +1,25 @@
 package com.apiumhub.androidcourse.adapter
 
-data class TodoItem(val title: String, val date: Long)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class TodoItem(val title: String?, val date: Long) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readLong()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeLong(date)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<TodoItem> {
+        override fun createFromParcel(parcel: Parcel): TodoItem = TodoItem(parcel)
+
+        override fun newArray(size: Int): Array<TodoItem?> = arrayOfNulls(size)
+    }
+}
