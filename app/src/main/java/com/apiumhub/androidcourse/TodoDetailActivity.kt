3235118b089c24c3ea0937cd833189f88da4f35a
Juan Jesús Fernández Madrid <juangefm@gmail.com>
2020-02-04 21:44:59 +0100

    add back navigation

--- app/src/main/java/com/apiumhub/androidcourse/TodoDetailActivity.kt
+++ app/src/main/java/com/apiumhub/androidcourse/TodoDetailActivity.kt
@@ -3,6 +3,7 @@ package com.apiumhub.androidcourse
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.apiumhub.androidcourse.adapter.TodoItem
 
@@ -11,10 +12,26 @@ class TodoDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo_detail)
+
+        val todoItem: TodoItem? = intent.extras?.getParcelable(ARG_TODO_ITEM_ID)
+        todoItem?.let {
+            supportActionBar?.title = it.title
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     companion object {
-        const val ARG_TODO_ITEM_ID = "arg.todo_item.id"
+        private const val ARG_TODO_ITEM_ID = "arg.todo_item.id"
 
         fun getCallingIntent(context: Context, todoItem: TodoItem): Intent {
             return Intent(context, TodoDetailActivity::class.java).apply {
